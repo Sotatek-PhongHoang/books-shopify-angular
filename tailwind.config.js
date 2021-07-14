b740942125340965d3eb6640b16c15180ad6c0de
Sotatek-PhongHoang <phong.hoang@sotatek.com>
2021-07-14 17:53:41 +0700

    update config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '900px',
+        xl: '1024px',
+        '2xl': '1280px'
+      },
+      padding: {
+        DEFAULT: '1rem',
+      }
+    }
   },
   variants: {
     extend: {},
